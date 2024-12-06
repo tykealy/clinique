bf353b41ff8fcf803a8a12e65f75d248a3b02fbe
Tykea Ly <tykeaboyloy@gmail.com>
2024-12-07 00:25:03 +0700

    close #36 add appointment card

--- app/javascript/controllers/new_appointment_modal_controller.js
+++ app/javascript/controllers/new_appointment_modal_controller.js
@@ -1,17 +1,27 @@
-// app/javascript/controllers/profile_preview_controller.js
-
 import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
   static targets = ["container", "button", "closeButton"];
 
   closeModal(e){
-    console.log("closeModal");
     this.containerTarget.classList.add("hidden");
   }
 
   toggleModal(e) {
-    console.log("toggleModal");
     this.containerTarget.classList.remove("hidden");
   }
 
+  // async show(e){
+  //   console.log(e.target.dataset.appointmentId);
+  //   const id = e.target.dataset.appointmentId;
+  //   const response = await fetch(`/admin/appointments/${id}`);
+
+  //   if(response.ok){
+  //     const data = await response.text();
+  //     this.appointmentCardTarget.innerHTML = data;
+  //   }
+  // }
+
+  // close(e) {
+  //   this.appointmentCardTarget.innerHTML = "";
+  // }
 }
