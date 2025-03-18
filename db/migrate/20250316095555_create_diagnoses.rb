9a482515a7160c73a1d13b36cbfaf48d254c18e2
Tykea Ly <tykeaboyloy@gmail.com>
2025-03-18 22:31:57 +0700

    close #185 config docker stg

--- db/migrate/20250316095555_create_diagnoses.rb
+++ db/migrate/20250316095555_create_diagnoses.rb
@@ -1,13 +1,18 @@
 class CreateDiagnoses < ActiveRecord::Migration[7.2]
   def change
-    create_table :diagnoses do |t|
-      t.string :name
-      t.string :code
-      t.string :description
-      t.integer :status, default: 1
-      t.references :clinic, null: false, foreign_key: true
-      t.timestamps
+    unless table_exists?(:diagnoses)
+      create_table :diagnoses do |t|
+        t.string :name
+        t.string :code
+        t.string :description
+        t.integer :status, default: 1
+        t.references :clinic, null: false, foreign_key: true
+        t.timestamps
+      end
     end
-    add_index :diagnoses, [:clinic_id, :name], unique: true
+
+    unless index_exists?(:diagnoses, [:clinic_id, :name], unique: true)
+      add_index :diagnoses, [:clinic_id, :name], unique: true
+    end
   end
 end
