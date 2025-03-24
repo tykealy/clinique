2bc719736f2bc8d4b5f713a40632dcfc6daf1d5c
Tykea Ly <tykeaboyloy@gmail.com>
2025-03-24 21:00:34 +0700

    close #205 added tooth highlighting

--- app/javascript/controllers/teeth_controller.js
+++ app/javascript/controllers/teeth_controller.js
@@ -1,24 +1,35 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["tooth", "diagnosisForm", "toothLabel", "noToothDiagnoses", "toothDeleteButton"]
+  static targets = ["tooth", "diagnosisForm", "toothLabel", "noToothDiagnoses", "toothDeleteButton", "teethContainer"]
 
   connect() {
-    console.log("Teeth controller connected")
+    this.selectedTooth = null;
+    this.highlightDiagnosedTeeth();
   }
 
   dbClickTooth(event) {
-    const toothNumber = event.currentTarget.dataset.toothNumber
-    const toothNumberInput = this.diagnosisFormTarget.querySelector('input[name="tooth_diagnosis[tooth_number]"]')
-
-    this.diagnosisFormTarget.classList.remove("hidden")
-    this.toothLabelTarget.textContent = `Tooth #${toothNumber}`
+    const clickedTooth = event.currentTarget;
+    const toothNumber = clickedTooth.dataset.toothNumber;
+    const patientDiagnosisId = this.teethContainerTarget.dataset.patientDiagnosisId;
+    
+    if (this.selectedTooth && this.selectedTooth !== clickedTooth) {
+      if (this.selectedTooth.getAttribute('data-diagnosed') !== 'true') {
+        this.unhighlightTooth(this.selectedTooth);
+      }
+    }
     if (this.hasNoToothDiagnosesTarget) {
       this.noToothDiagnosesTarget.classList.add("hidden")
     }
-    if (toothNumberInput) {
-      toothNumberInput.value = toothNumber
-    }
+    
+    this.selectedTooth = clickedTooth;
+    this.toothLabelTarget.textContent = `Tooth #${toothNumber}`
+    this.highlightTooth(clickedTooth);
+    
+    const diagnosisForm = this.diagnosisFormTarget;
+    diagnosisForm.classList.remove('hidden');
+
+    this.updateFormForTooth(toothNumber, patientDiagnosisId);
   }
 
   deleteToothDiagnosis(event) {
@@ -41,6 +52,56 @@ export default class extends Controller {
     })
     .catch(error => {
       console.error('Error:', error);
+    });
+  }
+
+  unhighlightTooth(tooth) {
+    const paths = tooth.querySelectorAll('svg path');
+    
+    if (paths.length >= 1) {
+      paths[0].setAttribute('style', 'fill:#fff');
+    }
+    
+    if (paths.length >= 2) {
+      paths[1].setAttribute('style', '');
+    }
+  }
+
+  highlightTooth(tooth) {
+    const paths = tooth.querySelectorAll('svg path');
+    
+    if (paths.length >= 1) {
+      paths[0].setAttribute('style', 'fill:#e6f7ff; stroke:#0099cc; stroke-width:1');
+    }
+    
+    if (paths.length >= 2) {
+      paths[1].setAttribute('style', 'fill:#ccf2ff');
+    }
+  }
+
+  updateFormForTooth(toothNumber, patientDiagnosisId) {
+    const form = this.diagnosisFormTarget.querySelector('form');
+    const toothNumberField = form.querySelector('[name="tooth_diagnosis[tooth_number]"]');
+    
+    if (toothNumberField) {
+      toothNumberField.value = toothNumber;
+    }
+  }
+
+  handleDiagnosisSaved(toothNumber) {
+    const diagnosedTooth = this.toothTargets.find(tooth => 
+      tooth.dataset.toothNumber === toothNumber.toString()
+    );
+    
+    if (diagnosedTooth) {
+      diagnosedTooth.setAttribute('data-diagnosed', 'true');
+    }
+  }
+
+  highlightDiagnosedTeeth() {
+    const diagnosedTeeth = this.element.querySelectorAll('[data-diagnosed="true"]');
+    diagnosedTeeth.forEach(tooth => {
+      this.highlightTooth(tooth);
     });
   }
 }
